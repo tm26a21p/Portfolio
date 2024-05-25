16cd51eb73af596fbbaae37fb390644e07b66f60
tm26a21p <louis.piochaud@epitech.eu>
2024-05-25 11:58:23 -0700

    fix: get for about and port 3000 -> 4444

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,30 @@
-use axum::{
-    routing::{get, post},
-    Router,
-    extract::Extension,
-};
+use axum::{extract::Extension, routing::get, Router};
 mod routes;
+mod state;
 mod templates;
 mod utils;
-mod state;
 use routes::*;
 use state::Common;
 
 #[tokio::main]
-async fn main() {
+async fn main()
+{
     tracing_subscriber::fmt::init();
 
     let state = Common::new();
 
     let app = Router::new()
         .route("/", get(index))
-        .route("/about", post(about))
+        .route("/about", get(about))
         .route("/metrics", get(metrics))
         .nest("/public", using_serve_dir())
         .layer(Extension(state.clone()));
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:4444")
         .await
-        .expect("Failed to bind port 3000");
-    println!("Listening on: http://{}", "localhost:3000");
-    axum::serve(listener, app.into_make_service()).await.expect("Server failed")
-    
+        .expect("Failed to bind port 4444");
+    println!("Listening on: http://{}", "localhost:4444");
+    axum::serve(listener, app.into_make_service())
+        .await
+        .expect("Server failed")
 }
