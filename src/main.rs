b29e3c0eea9225615971ca9f5192bc62f02d42f6
tm26a21p <louis.piochaud@epitech.eu>
2024-05-27 20:18:22 -0700

    feat: dotenv, octocrab (Github API in rust) and projects route

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,27 @@
 use axum::{extract::Extension, routing::get, Router};
+mod projects;
 mod routes;
 mod state;
 mod templates;
 mod utils;
 use routes::*;
 use state::Common;
+extern crate dotenv;
 
+use dotenv::dotenv;
 #[tokio::main]
 async fn main()
 {
     tracing_subscriber::fmt::init();
 
     let state = Common::new();
+    dotenv().ok();
 
     let app = Router::new()
         .route("/", get(index))
         .route("/home", get(index))
         .route("/metrics", get(metrics))
+        .route("/projects", get(projects))
         .nest("/public", using_serve_dir())
         .layer(Extension(state.clone()));
 
