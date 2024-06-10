f5d43660eccee4afa34c8acc4fc42f35279f1665
tm26a21p <louis.piochaud@epitech.eu>
2024-06-09 18:44:22 -0700

    fix: code formatting and renaming

--- src/routes.rs
+++ src/routes.rs
@@ -35,9 +35,9 @@ pub async fn metrics_page(
     let template = MetricsT {
         base,
         likes: 0,
-        likes_percent_over_last_month: 0.0,
+        likes_ratio_over_last_month: 0.0,
         views: 0,
-        view_percent_over_to_last_month: 0.0,
+        view_ratio_over_last_month: 0.0,
     };
     let reply_html = template.render().expect("Failed to render template");
     (StatusCode::OK, Html(reply_html).into_response())
@@ -51,7 +51,7 @@ pub async fn projects_page(
         title: state.name.clone() + " - Projects",
         daisy_theme: state.daisy_theme.clone(),
     };
-    let projects = Project::get_repos()
+    let projects = Project::get_repositories(state.octocrab)
         .await
         .expect("Failed to get repos from Github API");
     let template = ProjectsT { base, projects };
@@ -83,9 +83,7 @@ pub async fn playground_page(
         title: state.name.clone() + " - Playground",
         daisy_theme: state.daisy_theme.clone(),
     };
-    let template = PlaygroundT {
-        base
-    };
+    let template = PlaygroundT { base };
     let reply_html = template.render().expect("Failed to render template");
     (StatusCode::OK, Html(reply_html).into_response())
 }
