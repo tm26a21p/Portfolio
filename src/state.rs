bb404cae15f584384d6598a1d2cfc391b4fd9405
tm26a21p <louis.piochaud@epitech.eu>
2024-06-09 18:43:32 -0700

    fix: moving the github_token and octocrab instance in the Common state

--- src/state.rs
+++ src/state.rs
@@ -7,16 +7,26 @@ pub struct Common
     pub name: String,
     pub version: String,
     pub daisy_theme: String,
+    pub _github_token: String,
+    pub octocrab: octocrab::Octocrab,
 }
 
 impl Common
 {
     pub fn new() -> Self
     {
+        let github_token = std::env::var("GITHUB_TOKEN").expect("GITHUB_TOKEN not found.");
         Self {
             name: "Lpio".to_string(),
             version: "0.0.1".to_string(),
-            daisy_theme: random_daisy_theme()
+            daisy_theme: random_daisy_theme(),
+            _github_token: github_token.clone(),
+            octocrab: octocrab::Octocrab::builder()
+                .personal_token(
+                    github_token
+                )
+                .build()
+                .expect("Failed to create Octocrab instance."),
         }
     }
 }
